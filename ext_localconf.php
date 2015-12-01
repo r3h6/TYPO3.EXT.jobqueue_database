d0713153871ef3edf5b1895e2292420be65d36d1
r3h6 <r3h6@outlook.com>
2015-12-02 00:23:32 +0100

    [TASK] ExtConf

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,4 +4,4 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['EXT']['jobqueue']['TYPO3\\Jobqueue\\Queue\\DatabaseQueue'] = [];
+$GLOBALS['TYPO3_CONF_VARS']['EXT']['jobqueue']['TYPO3\\JobqueueDatabase\\Queue\\DatabaseQueue'] = [];
