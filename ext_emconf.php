934e8d7f4a76a7bb56efdb9edcc13d7eeb5ba322
r3h6 <r3h6@outlook.com>
2016-01-23 21:34:27 +0100

    [TASK] Set version to 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,11 +21,11 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '0.0.1',
+    'version' => '1.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2',
-            'jobqueue' => '0.0.1',
+            'typo3' => '7.6.0-7.6.99',
+            'jobqueue' => '1.0.0-1.0.99',
         ),
         'conflicts' => array(
         ),
